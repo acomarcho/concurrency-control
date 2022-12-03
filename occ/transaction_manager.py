7a9fb3febb915671b637d4048d814c22a8dba77e
acomarcho <acoxstpd@gmail.com>
2022-12-03 20:23:35 +0700

    Feat: Add comments for OCC

--- occ/transaction_manager.py
+++ occ/transaction_manager.py
@@ -25,29 +25,37 @@ class TransactionManager:
       task = self.schedule[idx]
       transaction = self.transactions[task['transaction']]
       if task['operation'] == 'write':
+        # Handle writes -- Tinggal dibaca saja!
         resource = self.resources[task['resource']]
         transaction.write(resource)
         res.append(f"W{task['transaction']}({task['resource']})")
         print(f"Transaction T{task['transaction']} writes {task['resource']}")
         idx += 1
       elif task['operation'] == 'read':
+        # Handle reads -- Tinggal ditulis saja!
         resource = self.resources[task['resource']]
         transaction.read(resource)
         res.append(f"R{task['transaction']}({task['resource']})")
         print(f"Transaction T{task['transaction']} reads {task['resource']}")
         idx += 1
       elif task['operation'] == 'commit':
+        # Saat commit, update validation_ts dan finish_ts.
+        # Anggapannya di sini adalah finish_ts = validation_ts untuk menyederhanakan.
         transaction.validation_ts = self.ts
         transaction.finish_ts = self.ts
 
         can_commit = True
         for key in self.transactions:
+          # Cek setiap transaksi yang ada.
           to_check = self.transactions[key]
           if to_check != transaction:
             if to_check.validation_ts and to_check.validation_ts < transaction.validation_ts:
+              # Jika ada sebuah transaksi yang waktu validasinya lebih dahulu,
               if to_check.finish_ts and to_check.finish_ts < transaction.start_ts:
+                # Memenuhi syarat pertama -- OK
                 continue
               elif to_check.finish_ts and transaction.start_ts < to_check.finish_ts and to_check.finish_ts < transaction.validation_ts:
+                # Memenuhi syarat kedua -- Perlu dicek apakah transaksi to_check pernah menulis resource yang dibaca oleh transaction
                 for r1 in to_check.written_resources:
                   if not can_commit:
                       break
@@ -55,6 +63,7 @@ class TransactionManager:
                     if not can_commit:
                       break
                     if r1 == r2:
+                      # Ada resource yang pernah ditulis oleh to_check yang dibaca oleh transaction --> Rollback!
                       conflicting_resource = ""
                       for resource in self.resources:
                         if self.resources[resource] == r1:
@@ -69,10 +78,12 @@ class TransactionManager:
                 break
           
         if can_commit:
+          # Jika diperbolehkan untuk commit,
           res.append(f"C{task['transaction']}")
           print(f"Transaction T{task['transaction']} commits")
           idx += 1
         else:
+          # Tidak diperbolehkan untuk commit! Lakukan rollback.
           res.append(f"A{task['transaction']}")
           print(f"Transaction T{task['transaction']} aborts")
           aborted_operations = []
@@ -80,8 +91,10 @@ class TransactionManager:
             if self.transactions[self.schedule[prev]['transaction']] == transaction:
               aborted_operations.append(self.schedule[prev])
 
+          # Mulai transaksi di timestamp baru
           self.transactions[task['transaction']] = Transaction(self.ts)
 
+          # Abort transaksi -- rollback dari awal sampai titik kini.
           for op in aborted_operations:
             transaction = self.transactions[op['transaction']]
             if op['operation'] == 'write':
